11d10c4928a12682d300ceb05c7810ee4406c52f
MiguelHerreraBejarano <makob811@gmail.com>
2023-09-18 18:38:44 -0500

    adding UserDetailPage styles

--- src/pages/UserDetailPage.tsx
+++ src/pages/UserDetailPage.tsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
-
+import './styles/UserDetailPage.css';
+import Userimg  from "./img/defaultUserImg.png";
 // React Router DOM Imports
 import { useNavigate, useParams } from 'react-router-dom';
 import { useSessionStorage } from '../hooks/useSessionStorage';
 import { getUserById } from '../services/usersService';
 import { AxiosResponse } from 'axios';
 import { User } from '../utils/types/User.type';
+import DashboardMenuLateral from '../components/dashboard/DashboardMenulateral';
 
 export const UserDetailPage = () => {
   let loggedIn = useSessionStorage('sessionJWTToken');
@@ -38,33 +40,63 @@ export const UserDetailPage = () => {
   // Variable to navigate
 
   return (
-    <div>
-      <h1>User Detail Page: {id}</h1>
-      {user ? (
-        <div className='user-data'>
-          <h2>{user.name}</h2>
-          <h3>{user.number}</h3>
-          <h3>{user.username}</h3>
-          <h3>{user.cedula}</h3>
-          <h3>{user.email}</h3>
-          <h3>{user.telefono}</h3>
-          <h3>{user.more_info}</h3>
-          {user.roles && user.roles.length > 0 && (
+    <div className=''>
+      <DashboardMenuLateral />
+      <div className=''>
+            {user ? (
+            <div className='UserCard'>
+                  <section className="usercard-user-section">
+                  <div className="usercard" data-state="#about">
+                      <div className="usercard-header">
+                        <div className="usercard-cover" ></div>
+                        <img className='usercard-Avatar' src={Userimg} alt="" width="90px" height="80px" />
+                        <h2 className="usercard-fullname">{user.name}</h2>
+                              {user.roles && user.roles.length > 0 && (
+                              <div>
+                                  {user.roles.map((role: any) => (
+                                    <h2 className='usercard-jobtitle' key={role._id}>{role.name}</h2>
+                                  ))}
+                              </div>
+                               )}
+                      </div>
+                      <div className="usercard-main">
+                        <div className="usercard-section is-active" >
+                          <div className="usercard-content-1">
+                            <div className="usercard-subtitle">Nombre de usuario</div>
+                              <p className="usercard-desc">{user.username}</p>
+                              <br></br>
+                            <div className="usercard-subtitle">Cédula</div>
+                              <p className="usercard-desc">{user.cedula}</p>
+                              <br></br>
+                            <div className="usercard-subtitle">Más Info</div>
+                              <p className="usercard-desc">{user.more_info}</p>     
+                          </div>     
+
+                          <div className="usercard-content-2"> 
+                              <div className="usercard-subtitle">Object id</div>
+                              <p className="usercard-desc">{id}</p>
+                              <br></br>
+                            <div className="usercard-subtitle">Teléfono</div>
+                              <p className="usercard-desc">{user.telefono}</p>
+                              <br></br>
+                            <div className="usercard-subtitle">Email</div>
+                                <p className="usercard-desc">{user.email}</p>
+                          </div>                
+                        </div>
+                        
+                      </div>
+                      <div className="usercard-buttons">
+                           <button className='usercard-button' data-section="#experience">Editar</button>
+                      </div>
+                    </div>
+                  </section>
+                  </div>           
+          ) : (
             <div>
-              <h3>Roles:</h3>
-              <ul>
-                {user.roles.map((role: any) => (
-                  <li key={role._id}>{role.name}</li>
-                ))}
-              </ul>
+              <h2>Loading data...</h2>
             </div>
-          )}
-        </div>
-      ) : (
-        <div>
-          <h2>Loading data...</h2>
-        </div>
-      )}
+             )}
+      </div>
     </div>
   );
 };

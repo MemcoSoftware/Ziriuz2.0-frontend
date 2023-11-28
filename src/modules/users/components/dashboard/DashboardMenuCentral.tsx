f6613835361200db0c7e50901671314ef666e0ed
Miguel Herrera <software@memcosas.com>
2023-11-28 00:16:53 -0500

    adding changes

--- src/modules/users/components/dashboard/DashboardMenuCentral.tsx
+++ src/modules/users/components/dashboard/DashboardMenuCentral.tsx
@@ -2,80 +2,61 @@ import React from 'react';
 import './styles/DashboardMenuCentral.css';
 import ZiriuzDashboardLogo from './img/ziriuzDashboardLogo.png'
 import { useNavigate } from 'react-router-dom';
-import  PersonIcon from '@mui/icons-material/Person';
-import ApartmentIcon from '@mui/icons-material/Apartment';
-import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
-import ShieldIcon from '@mui/icons-material/Shield';
+import DashboardMenuCentralEquipos from '../../../equipos/components/dashboard_equipos/DashboardMenuCentralEquipos';
+import DashboardMenuCentralGenerales from './DashboardMenuCentralGenerales';
 const DashboardMenuCentral = () => {
     const navigate = useNavigate();
 
     return (
        
-      <div className='DashboardMenuCentral'>
-              <section className="DashboardMenuCentral-LogoPrincipal">
-                  <div className="DashboardMenuCentral-logo">
-                          <a className="DashboardMenuCentral-a" href="">
-                              <img  src={ZiriuzDashboardLogo} alt="" width = "250px" height = "150px" />
-                              <span></span>
-                              <span></span>
-                              <span></span>
-                              <span></span>
-                          </a>
-                  </div>
-              </section>
-              <section className="DashboardMenuCentral-Block">
-                      <nav className="DashboardMenuCentral-nav">
+        <div className='DashboardMenuCentral'>
+                <section className="DashboardMenuCentral-LogoPrincipal">
+                    <div className="DashboardMenuCentral-logo">
+                            <a className="DashboardMenuCentral-a" href="">
+                                <img  src={ZiriuzDashboardLogo} alt="" width = "250px" height = "150px" />
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                            </a>
+                    </div>
+                </section>
+                {/* <section className="DashboardMenuCentral-Block"> */}
+                    <nav className="DashboardMenuCentral-nav">
+                        {/* <div className='menuGenerales'>
+                            <DashboardMenuCentralGenerales />
+                        </div>
+                        <div className='MenuEquipos'>
+                            <DashboardMenuCentralEquipos/>
+                        </div> */}
                     
-                          <ul className="DashboardMenuCentral-nav-ul">
-                          <h1 className='DashboardMenuCentral-title'> Generales</h1>
-                              <button className="DashboardMenuCentral-button" onClick={() => navigate('/users')}>
-                              <li>
-                                  <div className="DashboardMenuCentral-nav-icon">
-                                  <i className='DashboardMenuCentral-nav-icon-i'>    
-                                    <PersonIcon/>                
-                                  </i>
-                                 <p className="DashboardMenuCentral-p">Usuarios</p> 
-                                  </div>
-                              </li>
-                              </button>
-                              <button className="DashboardMenuCentral-button" onClick={() => navigate('/sedes')}>
-                              <li>
-                                  <div className="DashboardMenuCentral-nav-icon">
-                                      <i className='DashboardMenuCentral-nav-icon-i'> 
-                                          {/* <img  alt=""width="40px" height="30px"/> */}
-                                          <ApartmentIcon/>
-                                                
-                                      </i>
-                                      <p className="DashboardMenuCentral-p">Sedes</p>
-                                  </div>
-                              </li>
-                              </button>
-                              <button className="DashboardMenuCentral-button" onClick={() => navigate('/clientes')}>
-                              <li>
-                                  <div className="DashboardMenuCentral-nav-icon">
-                                      <i className='DashboardMenuCentral-nav-icon-i'>
-                                           {/* <img  alt="" width="40px" height="30px"/> */}
-                                           <AccountBalanceIcon/>
-                                      </i>
-                                      <p className="DashboardMenuCentral-p">  Clientes</p>
-                                  </div>
-                              </li>
-                              </button>
-                              <button className="DashboardMenuCentral-button">
-                              <li>
-                                  <div className="DashboardMenuCentral-nav-icon">
-                                  <i className='DashboardMenuCentral-nav-icon-i'>
-                                      {/* <img  alt="" width="40px" height="30px"/> */}
-                                      <ShieldIcon/>
-                                  </i>
-                                  <p className="DashboardMenuCentral-p">Permisos </p>  
-                                  </div>
-                              </li>
-                              </button>
-                          </ul>
-              </nav>
-              </section>
-      </div>
+                        <div className="wrapper">
+                            <div className="acordeon-core">
+                                <div className="acordeon">
+                                    <input id="acordeon-1" type="checkbox" name="acordeons"/>
+                                    <label htmlFor="acordeon-1">Generales</label>
+                                        <div className="acordeon-content">
+                                            <DashboardMenuCentralGenerales />                     
+                                        </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div className="wrapper">
+                            <div className="acordeon-core">
+                                <div className="acordeon">
+                                    <input id="acordeon-2" type="checkbox" name="acordeons"/>
+                                    <label htmlFor="acordeon-2">Equipos</label>
+                                        <div className="acordeon-content">
+                                        <DashboardMenuCentralEquipos/>                    
+                                        </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </nav>
+                {/* </section> */}
+        </div>
     ); 
   };
   

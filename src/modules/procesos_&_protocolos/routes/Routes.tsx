f73c4331944de5fcee95fba5de6d9bb0a3ab56a1
MiguelHerreraBejarano <makob811@gmail.com>
2023-12-28 12:55:27 -0500

    adding procesos_&_protcolos module

---
+++ src/modules/procesos_&_protocolos/routes/Routes.tsx
@@ -0,0 +1,13 @@
+
+// src/modules/equipos/routes/Routes.tsx
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+// import { EquiposPages } from '../pages/EquiposPages'; 
+
+export const Procesos_Protocolos_Router = () => {
+  return (
+    <Routes>
+      {/* <Route path="/equipos" element={<EquiposPages />} /> */}
+    </Routes>
+  );
+};

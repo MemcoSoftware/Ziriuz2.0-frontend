329050fa9c077b7c3654c6e7e6c0c2cbb7946e07
MiguelHerreraBejarano <makob811@gmail.com>
2024-01-30 21:46:20 -0500

    adding searchSolicitudesService

---
+++ src/modules/solicitudes/services/searchSolicitudesService.ts
@@ -0,0 +1,30 @@
+// searchSolicitudesService.ts
+import axios from '../../../utils/config/axios.config';
+import { AxiosRequestConfig, AxiosResponse } from 'axios';
+
+export const searchSolicitudesServiciosByKeyword = async (token: string, keyword: string) => {
+  const options: AxiosRequestConfig = {
+    headers: {
+      'x-access-token': token,
+    },
+  };
+
+  const requestBody = {
+    keyword: keyword,
+  };
+
+  try {
+    const response = await axios.post('/search/solicitudes/solicitudes-servicios', requestBody, options);
+    return response.data;
+  } catch (error: any) {
+    if (error.response) {
+      const { status } = error.response as AxiosResponse;
+      if (status === 500) {
+        // Token inválido o expirado
+        // Redirigir al usuario a la página de inicio de sesión (/login)
+        window.location.href = '/login';
+      }
+    }
+    throw error;
+  }
+};
